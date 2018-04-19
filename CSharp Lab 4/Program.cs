455ef9b8f0bb3f8570d2a9a5bd8112454ed5bb81
Brian Johnson <brian.johnson5145@gmail.com>
2018-04-18 19:29:22 -0500

    Add project files.

---
+++ CSharp Lab 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSharp_Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int largestNumber;
+            int smallestNumber;
+            int currentNumber;
+
+            Console.Write("Please enter a number (0 to quit): ");
+            currentNumber = Int32.Parse(Console.ReadLine());
+            smallestNumber = currentNumber;
+            largestNumber = currentNumber;
+
+            while ( currentNumber !=0)
+            {
+                if (currentNumber > largestNumber)
+                {
+                    largestNumber = currentNumber;
+                }
+                if (currentNumber < smallestNumber)
+                {
+                    smallestNumber = currentNumber;
+                }
+                Console.Write("Please enter another number(0 to quit): ");
+                currentNumber = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Your smallest number is: " + smallestNumber);
+            Console.WriteLine("Your largest number is: " + largestNumber);
+            Console.ReadLine();
+        }
+    }
+}
